d9d9b7e069ec621f94dc856a913f41c6c7525062
Jigyass <jigyas@icloud.com>
2025-09-05 10:00:46 -0500

    f

--- Lab1:Debuggers/valgrind_test.c
+++ Lab1:Debuggers/valgrind_test.c
@@ -15,7 +15,8 @@ void* still_reachable;
 void* possibly_lost;
 
 int main() {
-  int uninitialized_variable; // This variable is never given a value.
+  // Fix: initialize to 0
+  int uninitialized_variable = 0; // This variable is never given a value.
 
   for (; uninitialized_variable < 100; uninitialized_variable++) {
     void** definitely_lost = (void**) malloc(sizeof(void*)); // allocate a
@@ -25,6 +26,10 @@ int main() {
     *definitely_lost = (void*) malloc(7); // Give the pointer something else to
                                           // point to on the heap. This will be
                                           // indirectly lost.
+
+    // Fix: on each iteration, free *definitely_lost, then free definitely_lost
+    free(*definitely_lost);
+    free(definitely_lost);
   }
 
   // At this point, definitely_lost is out of scope and we can no longer free
@@ -40,6 +45,12 @@ int main() {
                       // but nothing points to the front of the block. This is
                       // very odd behavior and usually is a memory leak (but not
                       // always).
+
+  // Fix: free still_reachable before program goes out of scope
+  free(still_reachable);
+  
+  // Fix: point the 'possibly_lost' pointer back to start of allocated block, then free
+  free(possibly_lost - 4);
 
   return 0;
 }
